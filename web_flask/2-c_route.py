785a0a6b48f9002dda8aef75e122c78b00f8af31
Navygeta <tinashenavonika@gmail.com>
2024-02-19 04:56:01 +0200

    c route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,7 +6,7 @@ Simple Flask web application.
 This application listens on 0.0.0.0, port 5000, and has three routes.
 The route ("/") displays the message "Hello HBNB!".
 The route ("/hbnb") displays the message "HBNB".
-The route ("/c/<text>") displays "C" followed by the value of the text variable.
+The route ("/c/<text>") displays "C" followed by the value of the text variable
 
 Usage:
     Run this script and access the web application at http://0.0.0.0:5000/
@@ -16,15 +16,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Display "Hello HBNB!" on the root route."""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Display "HBNB" on the "/hbnb" route."""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def ctext(text):
@@ -39,6 +42,7 @@ def ctext(text):
     """
     text = text.replace("_", " ")
     return "C {}".format(text)
+
 
 if __name__ == '__main__':
     app.run()
